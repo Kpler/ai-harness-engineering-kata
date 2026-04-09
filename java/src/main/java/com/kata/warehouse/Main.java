1abe22df9580de8cd9dc469a5e445c09731cebc7
Léo <ldureuil@kpler.com>
2026-04-09 18:54:10 +0800

    chore: delete java

--- java/src/main/java/com/kata/warehouse/Main.java
+++
@@ -1,9 +0,0 @@
-package com.kata.warehouse;
-
-public class Main {
-  public static void main(String[] args) {
-    WarehouseDeskApp app = new WarehouseDeskApp();
-    app.seedData();
-    app.runDemoDay();
-  }
-}
